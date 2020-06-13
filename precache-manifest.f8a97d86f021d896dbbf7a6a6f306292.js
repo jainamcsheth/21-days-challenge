8b34c06f63cc0a40e29f49fe6f1530f44e85be89
Jainam Sheth <jainamcsheth@gmail.com>
2020-06-13 16:31:00 +0530

    Updates

---
+++ precache-manifest.f8a97d86f021d896dbbf7a6a6f306292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4f7d736e40834f77d1b25805e574a9",
+    "url": "/21-days-challenge/index.html"
+  },
+  {
+    "revision": "359a4ddd2ab6c583be00",
+    "url": "/21-days-challenge/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2cd8681c885fec55d69d",
+    "url": "/21-days-challenge/static/js/2.ac6258d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/21-days-challenge/static/js/2.ac6258d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359a4ddd2ab6c583be00",
+    "url": "/21-days-challenge/static/js/main.414dac72.chunk.js"
+  },
+  {
+    "revision": "d338a2337a7ad8a4e824",
+    "url": "/21-days-challenge/static/js/runtime-main.5d5e4548.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/21-days-challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
